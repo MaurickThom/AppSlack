f126dc8e246060634593b84cf467615a553c5dff
MaurickThom <thomtwd@gmail.com>
2019-08-11 23:47:50 -0500

    [parsing JSON]

--- extras/webSocket/js/scripts.js
+++ extras/webSocket/js/scripts.js
@@ -4,11 +4,18 @@ const CONNECT = document.getElementById('connect'),
     DISCONNECT = document.getElementById('disconnect'),
     SEND = document.getElementById('send'),
     mainContainer = document.querySelector('#main-container'),
-    message = document.getElementById('message'),
+    name = document.getElementById('name'),
+    msg = document.getElementById('message'),
     chat = document.getElementById('chat')
 
 const setText = message => (
     chat.insertAdjacentHTML('beforeend',`<div>${message}</div>`)
+)
+
+const setMessage = message => (
+    chat.insertAdjacentHTML('beforeend',`
+        <div>${message.name} : ${message.msg}</div>
+    `)
 )
 
 mainContainer.addEventListener('click', e => {
@@ -21,14 +28,18 @@ mainContainer.addEventListener('click', e => {
             ws.onclose = () => setText('DISCONNECTED')
             ws.onerror = err => setText(`ERROR ${err}`)
             ws.onmessage = message =>(
-                setText(message.data)
+                setMessage(JSON.parse(message.data))
             )
             break
         case DISCONNECT:
             ws.close()
             break
         case SEND:
-            ws.send(message.value)
+            let message = {
+                name :name.value,
+                msg = msg.value
+            }
+            ws.send(JSON.stringify(message))
             break
         default:
             break

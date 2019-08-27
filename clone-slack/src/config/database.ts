615999ab30caf789a27f123c337a57afe494be1c
MaurickThom <thomtwd@gmail.com>
2019-08-27 00:54:44 -0500

    adding attribute

--- clone-slack/src/config/database.ts
+++ clone-slack/src/config/database.ts
@@ -2,9 +2,10 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 dotenv.config()
 mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology:', true);
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.URL_DB as string, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 .then(db => console.log('DB is connected'))
 .catch(err => console.error(err));
